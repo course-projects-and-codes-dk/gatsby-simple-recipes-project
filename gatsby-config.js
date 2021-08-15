b38474680802543b87ad7da903c4fa4732014616
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-08-15 14:30:00 +0530

    added site meta data to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,18 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Simply Recipes",
+    description: "Nice and clean recipes site",
+    author: "Deekayy",
+    person: { name: "John", age: 32 },
+    simpleData: ["item 1", "item 2"],
+    complexData: [
+      { name: "John", age: 32 },
+      { name: "Susan", age: 27 },
+    ],
+  },
+
   plugins: [
     `gatsby-plugin-styled-components`,
     `gatsby-plugin-image`,
