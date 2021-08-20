b4d5b3b43090f02b3f54336a7a6c63352c42bfc3
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-08-21 01:32:44 +0530

    dynamic single tag page completed

--- src/templates/tag-template.js
+++ src/templates/tag-template.js
@@ -1,11 +1,44 @@
 import React from "react"
+import { graphql } from "gatsby"
+import RecipesList from "../components/RecipesList"
+import Layout from "../components/Layout"
 
-const TagTemplate = () => {
+const TagTemplate = props => {
+  // get filtered recipes from props/query
+  const recipes = props.data.allContentfulRecipe.nodes
+
+  // get tag name from the variable that gastby passes to query
+  const tagName = props.pageContext.tag
+
+  // jsx
   return (
-    <div>
-      <h2>tag template page</h2>
-    </div>
+    <Layout>
+      <main className="page">
+        <h2>{tagName}</h2>
+        <div className="tag-recipes">
+          <RecipesList recipes={recipes} />
+        </div>
+      </main>
+    </Layout>
   )
 }
 
+export const query = graphql`
+  query GetRecipeByTag($tag: String) {
+    allContentfulRecipe(
+      sort: { fields: title, order: ASC }
+      filter: { content: { tags: { eq: $tag } } }
+    ) {
+      nodes {
+        id
+        title
+        cookTime
+        prepTime
+        image {
+          gatsbyImageData(layout: CONSTRAINED, placeholder: TRACED_SVG)
+        }
+      }
+    }
+  }
+`
 export default TagTemplate
