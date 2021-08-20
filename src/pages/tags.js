3b6fb416bd1248e9d1d2bef85ff79a1a6d8c1f5a
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2021-08-20 14:01:55 +0530

    tag page completed

--- src/pages/tags.js
+++ src/pages/tags.js
@@ -1,12 +1,48 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { Link, graphql } from "gatsby"
+import setupTags from "../utils/setupTags"
 
-const Tags = () => {
+const Tags = ({
+  data: {
+    allContentfulRecipe: { nodes },
+  },
+}) => {
+  // get unique tags from utils
+  const newTags = setupTags(nodes)
+
+  // jsx
   return (
     <Layout>
-      <h1>tags page</h1>
+      <main className="page">
+        <section className="tags-page">
+          {newTags.map((tag, index) => {
+            // destrcuture tag array
+            const [text, value] = tag
+
+            return (
+              <Link key={index} to={`/${text}`} className="tag">
+                <h5>{text}</h5>
+                <p>{value} recipe</p>
+              </Link>
+            )
+          })}
+        </section>
+      </main>
     </Layout>
   )
 }
+
+export const query = graphql`
+  {
+    allContentfulRecipe {
+      nodes {
+        content {
+          tags
+        }
+      }
+    }
+  }
+`
 
 export default Tags
